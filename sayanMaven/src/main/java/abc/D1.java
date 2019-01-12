1bcaca94fc37b4e6afa9ca6c435fee6df49a0bf1
mdshamim3134 <mdshamim3134@gmail.com>
2019-01-12 17:35:20 -0500

    twelve commit

---
+++ sayanMaven/src/main/java/abc/D1.java
@@ -0,0 +1,16 @@
+package abc;
+
+
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+
+public class D1 {
+	WebDriver dr;
+	@Test
+	public void clickOnShirt() {
+		System.out.println("abc");
+	}
+
+}
